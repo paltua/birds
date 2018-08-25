5cbb16a99645f6400f0fa2ad903041fe024755ad
unknown <paltua@gmail.com>
2018-08-25 15:25:40 +0530

    status changed

---
+++ db/birds_2018_08_25.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `animal_master` ADD `am_code` VARCHAR(20) NOT NULL AFTER `user_id`, ADD UNIQUE (`am_code`);
+
+CREATE TABLE `contact_us` ( `con_id` BIGINT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `mobile` BIGINT NOT NULL , `desc` TEXT NOT NULL , `created_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`con_id`)) ENGINE = InnoDB;
+
+ALTER TABLE `contact_us` CHANGE `desc` `desccription` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
+ALTER TABLE `comments` CHANGE `com_id` `com_id` BIGINT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `animal_master_images` ADD `ami_default` TINYINT NOT NULL DEFAULT '0' AFTER `ami_created_date`;
