909e0ad237856a84cdedb98ddddaf1c8fb01c703
unknown <paltua@gmail.com>
2018-08-28 22:29:05 +0530

    email change

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,7 +84,7 @@ defined('FOPEN_READ_WRITE_CREATE_STRICT')       OR define('FOPEN_READ_WRITE_CREA
 */
 
 defined('ADMIN_NAME')      OR define('ADMIN_NAME', 'Admin'); // highest automatically-assigned error code
-defined('ADMIN_EMAIL')     OR define('ADMIN_EMAIL', 'parrotdipankar@gmail.com');//parrotdipankar@gmail.com
+defined('ADMIN_EMAIL')     OR define('ADMIN_EMAIL', 'srvbera@gmail.com');//parrotdipankar@gmail.com
 defined('SUPPORTEMAIL')    OR define('SUPPORTEMAIL', 'info@parrotdipankar.com');
 defined('THEME') OR define('THEME', 'theme1');
 defined('SITENAME') OR define('SITENAME', 'parrotdipankar.com');
