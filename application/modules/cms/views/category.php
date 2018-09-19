92c2a7cf95c3ffa4f586be614571c81f42d0e95d
unknown <paltua@gmail.com>
2018-09-20 00:03:08 +0530

    PUBLISH LISTING ADDED

---
+++ application/modules/cms/views/category.php
@@ -0,0 +1,27 @@
+<section class="inner-top-cat">
+  <div class="container">
+    <div class="category-circle carousel-7 owl-carousel owl-theme">
+      <?php if(count($category) > 0){
+            foreach ($category as $key => $value) {
+          ?>
+        <div class="item">
+          <figure>
+            <div class="circle-layout">
+              <?php 
+              $imagePath = base_url('public/'.THEME.'/images/no-image.jpg');
+              if($value->image_name != ''){
+                $imagePath = base_url(UPLOAD_CAT_PATH.$value->image_name);
+              }?>
+              <img src="<?php echo $imagePath;?>" alt="<?php echo $value->acmd_name;?>">
+              <figcaption>
+                <!-- <button><i class="lnr lnr-plus-circle"></i></button> -->
+                <a href="<?php echo base_url('user/product/search/'.$value->acm_id);?>" class="button"><i class="lnr lnr-plus-circle"></i></a>
+              </figcaption>
+            </div>                  
+          </figure>
+          <h3><a href="<?php echo base_url('user/product/search/'.$value->acm_id);?>"><?php echo $value->acmd_name;?></a></h3>
+        </div>
+        <?php } } ?>
+    </div>
+  </div>
+</section>
--- application/modules/user/views/product/search.php
+++ application/modules/user/views/product/search.php
@@ -80,32 +80,10 @@
 		</div>
 	</div>
 </section>
-<section class="inner-top-cat">
-  <div class="container">
-    <div class="category-circle carousel-7 owl-carousel owl-theme">
-      <?php if(count($category) > 0){
-            foreach ($category as $key => $value) {
-          ?>
-        <div class="item">
-          <figure>
-            <div class="circle-layout">
-              <?php 
-              $imagePath = base_url('public/'.THEME.'/images/add-image.jpg');
-              if($value->image_name != ''){
-                $imagePath = base_url(UPLOAD_CAT_PATH.$value->image_name);
-              }?>
-              <img src="<?php echo $imagePath;?>" alt="<?php echo $value->acmd_name;?>">
-              <figcaption>
-                <button><i class="lnr lnr-plus-circle"></i></button>
-              </figcaption>
-            </div>                  
-          </figure>
-          <h3><a href="<?php echo base_url('user/product/search/'.$value->acm_id);?>"><?php echo $value->acmd_name;?></a></h3>
-        </div>
-        <?php } } ?>
-    </div>
-  </div>
-</section>
+
+
+<?php $this->load->view('cms/category');?>
+
 <section class="inner-layout">
 	<div class="container">		
 		<div class="inner-content">
