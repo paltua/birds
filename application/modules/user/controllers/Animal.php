be6de3ae78ac2f15d0042c3b3ec6f238ee88b646
unknown <paltua@gmail.com>
2018-09-20 00:44:30 +0530

    PUBLISH LISTING ADDED

--- application/modules/user/controllers/Animal.php
+++ application/modules/user/controllers/Animal.php
@@ -113,6 +113,19 @@ class Animal extends MY_Controller {
         }
     }
 
+    private function resizeImage(){
+        $config['image_library'] = 'gd2';
+        $config['source_image'] = '/path/to/image/mypic.jpg';
+        $config['create_thumb'] = TRUE;
+        $config['maintain_ratio'] = TRUE;
+        $config['width']         = 75;
+        $config['height']       = 50;
+
+        $this->load->library('image_lib', $config);
+
+        $this->image_lib->resize();
+    }
+
 	public function edit($am_id = 0){
         $status = '';
 		$msg = '';
