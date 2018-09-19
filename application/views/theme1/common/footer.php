4f6f686d9d855bcb48b301ef2ff1c0117b5e3497
unknown <paltua@gmail.com>
2018-09-20 00:34:15 +0530

    PUBLISH LISTING ADDED

--- application/views/theme1/common/footer.php
+++ application/views/theme1/common/footer.php
@@ -1,4 +1,4 @@
-
+<!-- 
 
 	<div class="outer-container clearfix">
 		<div class="row">
@@ -15,6 +15,26 @@
 			</div>
 			<div class="col-lg-3 col-md-16">
 				<?php $this->load->view(THEME.'/common/socialLink');?>
+			</div>
+		</div>
+	</div> -->
+
+	<div class="outer-container clearfix">
+		<div class="row">
+			<div class="col-lg-12 text-center"><p>We only deal with EXOTIC BIRDS and other pets species which are LEGAL IN INDIA, AS PER INDIAN WIELD LIFE ACT.</p></div>
+			<div class="col-lg-3 col-md-12"><p>Copyright © <?php echo date("Y");?> ParrotDipankar</p></div>
+			<div class="col-lg-6 col-md-12">
+				<div class="botm-links">
+					<ul>
+						<li><a href="<?php echo base_url('cms/disclaimer');?>">Disclaimer</a></li>
+						<li><a href="<?php echo base_url('cms/google_privacy_policy');?>">Google Adsense Privacy Policy</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-16">
+				<ul class="ft-social">
+					<?php $this->load->view(THEME.'/common/socialLink');?>
+				</ul>
 			</div>
 		</div>
 	</div>
--- application/views/theme1/common/socialLink.php
+++ application/views/theme1/common/socialLink.php
@@ -1,7 +1,6 @@
-<ul class="ft-social">
+
 	<li class="fb"><a href="javascript:void(0)">Facebook</a></li>
 	<li class="twt"><a href="javascript:void(0)">Twitter</a></li>
 	<li class="inst"><a href="javascript:void(0)">Instagram</a></li>
 	<li class="linkd"><a href="javascript:void(0)">Linkdin</a></li>
 	<li class="utube"><a href="javascript:void(0)">YouTube</a></li>
-</ul>
