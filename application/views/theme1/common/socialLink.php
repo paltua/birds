488751db7680af5fbba525ba1b10b186c1e20042
unknown <paltua@gmail.com>
2018-10-11 23:05:15 +0530

    added social midea link

--- application/views/theme1/common/socialLink.php
+++ application/views/theme1/common/socialLink.php
@@ -1,6 +1,6 @@
 
-	<li class="fb"><a href="javascript:void(0)">Facebook</a></li>
-	<li class="twt"><a href="javascript:void(0)">Twitter</a></li>
-	<li class="inst"><a href="javascript:void(0)">Instagram</a></li>
+	<li class="fb"><a href="https://m.facebook.com/Parrot-Dipankar-Family-238127920161260/?ref=bookmarks">Facebook</a></li>
+	<li class="twt"><a href="https://twitter.com/parrotdipankar">Twitter</a></li>
+	<li class="inst"><a href="https://www.instagram.com/parrotdipankar/">Instagram</a></li>
 	<li class="linkd"><a href="javascript:void(0)">Linkdin</a></li>
-	<li class="utube"><a href="javascript:void(0)">YouTube</a></li>
+	<li class="utube"><a href="www.youtube.com/c/PARROTDIPANKAR">YouTube</a></li>
