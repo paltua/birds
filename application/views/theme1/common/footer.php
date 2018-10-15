df2d50ddfba626454a374f7cf61551a004f4305e
unknown <paltua@gmail.com>
2018-10-15 19:26:31 +0530

    google addsence added

--- application/views/theme1/common/footer.php
+++ application/views/theme1/common/footer.php
@@ -18,4 +18,11 @@
 			</div>
 		</div>
 	</div>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-3375071677065247",
+		enable_page_level_ads: true
+	});
+	</script>
 	
