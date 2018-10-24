f9690bbb1148ffa2381060a294a327958dd4bc9d
unknown <paltua@gmail.com>
2018-10-24 22:36:16 +0530

    deleted some pages

--- application/modules/user/config/autoload.php
+++ application/modules/user/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('data');
+$autoload['helper'] = array();
 
 /*
 | -------------------------------------------------------------------
