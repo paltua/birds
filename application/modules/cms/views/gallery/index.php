4996f3502e0a233b0f10e8b2552095d97beb5eaf
unknown <paltua@gmail.com>
2018-10-30 08:25:57 +0530

    gallery added

--- application/modules/cms/views/gallery/index.php
+++ application/modules/cms/views/gallery/index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="<?php echo base_url('public/'.THEME.'/');?>css/jquery.fancybox.css"/>
 <section class="innerbanner">
   <div class="banner-cont">
     <h1 class="title">Gallery</h1>
@@ -11,23 +12,39 @@
 </section>
 
 <?php $this->load->view('cms/category');?>
-
 <section class="inner-layout">
   <div class="container">   
     <div class="inner-content">
-      <div class="gridwrap">
-        <div class="two-grid secleft">
-            <div class="img-grid clearfix">
+      <div class="section homeGetTouch" id="section2">
+        <div class="gridwrap">
+            <div class="two-grid secleft">
+                <div class="img-grid clearfix">
                 <?php if(count($gallery) > 0){?>
-                <ul>
-                    <?php foreach ($gallery as $key => $value) {?>
-                    <li><img src="<?php echo base_url('uploads/gallery/thumb/'.$value->g_path);?>" alt=""/></li>
-                    <?php }?>
-                </ul>
-                <?php } ?>
-            </div>
+                  <ul>
+                      <?php foreach ($gallery as $key => $value) {?>
+                        <li><a href="<?php echo base_url('uploads/gallery/'.$value->g_path);?>" data-fancybox="gallery"><img src="<?php echo base_url('uploads/gallery/thumb/'.$value->g_path);?>" alt=""/></a></li>
+                      <?php }?>
+                  </ul>
+                  <?php } ?>
+                </div>
+            </div>  
         </div>
       </div>
     </div>
   </div>
-</section>
+  </section>  
+  <script src="<?php echo base_url('public/'.THEME.'/');?>js/jquery.fancybox.js"></script>
+  <script type="text/javascript">
+    $('[data-fancybox="gallery"]').fancybox({
+      buttons : [
+        'share',
+        'fullScreen',
+        'close'
+      ],
+      thumbs : {
+        autoStart : true
+      }
+    });
+
+</script>
+
