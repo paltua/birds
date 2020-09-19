23cc4a50786c67618b36b89f7139804fa29eb9c9
Ranaji Das <paltua@gmail.com>
2020-09-19 14:18:18 +0530

    added blog list chnages

--- application/modules/cms/views/blog/index.php
+++ application/modules/cms/views/blog/index.php
@@ -14,6 +14,29 @@
         <div class="inner-content">
             <div class="product-listing-layout">
                 <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 ">
+                        <div class="product-item">
+                            <div id="horizontalTab" class="pd-list-tab">
+                                <div class="resp-tabs-container">
+                                    <div>
+                                        <div class="row" id="productListDivId">
+                                            <?php 
+											$viewData['list'] = [];
+											$this->load->view('cms/blog/list_row', $viewData);
+											?>
+                                        </div>
+                                        <div class="buttonLoadMoreClass">
+                                            <?php if($prodListCount > $limit['perPage']){?>
+                                            <a href="javascript:void(0);" id="loadMoreId"
+                                                class="btn btn-primary float-right">Load More</a>
+                                            <?php } ?>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ application/modules/cms/views/blog/list_row.php
@@ -0,0 +1,73 @@
+<?php if ( count( $list ) > 0 ) {
+    foreach ( $list as $key => $value ) {
+        ?>
+<div class='col-md-12 col-sm-6 col-xs-12'>
+    <div class='pd-item-box'>
+        <figure>
+            <span class='img-box'>
+                <span class='img-inner'>
+                    <?php
+        $imagePath = base_url( 'public/'.THEME.'/images/cockatiel_01_img.jpg' );
+        if ( $value->ami_path != '' ) {
+            $imagePath = base_url( UPLOAD_PROD_PATH.'thumb/'.$value->ami_path );
+        }
+        ?>
+                    <a href="<?php echo base_url('user/product/details/'.$value->am_id);?>">
+                        <img src="<?php echo $imagePath;?>" />
+                    </a>
+                </span>
+            </span>
+            <figcaption>
+                <div class='content-item item-left'>
+                    <div>
+                        <h3><a href="<?php echo base_url('user/product/details/'.$value->am_id);?>"><?php echo $value->amd_name;
+        ?> : <?php echo $value->acmd_name;
+        ?></a></h3>
+                        <h4><?php //echo $value->amd_short_desc;
+        if ( strlen( $value->amd_short_desc ) >= 280 ) {
+            echo substr( $value->amd_short_desc, 0, 275 ).'...';
+        } else {
+            echo $value->amd_short_desc;
+        }
+        ?></h4>
+                        <h5><span class='location'><?php echo showLocation( $value->country_name, $value->state_name, $value->city_name );
+        ?></span>
+                            <span class='publshby'>Published by <b><?php echo ( $value->am_user_type == 'admin' )?'Company': $value->user_name;
+        ?></b></span>
+
+                        </h5>
+                        <?php if ( $value->buy_or_sell != '' ) {
+            ?>
+                        <span class='badge badge-pill badge-warning'>
+                            <i class='fa fa-tag'></i><?php echo ( $value->buy_or_sell == 'buy'?'To ':'For ' ).ucfirst( $value->buy_or_sell );
+            ?>
+                        </span>
+                        <?php }
+            ?>
+
+                    </div>
+                </div>
+                <div class='content-item item-right'>
+                    <div>
+                        <h2>Rs. <?php echo $value->amd_price;
+            ?></h2>
+                        <a href="<?php echo base_url('user/product/details/'.$value->am_id);?>"
+                            class='btn btn-danger'>View</a>
+                        <h6><?php echo $value->acmd_name;
+            ?></h6>
+                        <span class='publsh-date'>published
+                            <span><?php echo getViewDate( $value->am_created_date );
+            ?></span>
+                        </span>
+                        <span class='viewed'>viewed
+                            <span><?php echo $value->am_viewed_count;
+            ?>+</span>
+                        </span>
+                    </div>
+                </div>
+            </figcaption>
+        </figure>
+    </div>
+</div>
+<?php }
+        } ?>
