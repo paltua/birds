23c87c0bac8cb5b0b8394f631ebf8db4d39276e6
Ranaji Das <paltua@gmail.com>
2020-09-24 11:51:29 +0530

    charity added

--- application/modules/cms/controllers/Pd_charitable_trust.php
+++ application/modules/cms/controllers/Pd_charitable_trust.php
@@ -1,35 +1,38 @@
 <?php
-defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pd_charitable_trust extends MY_Controller {
+class Pd_charitable_trust extends MY_Controller
+{
     public $controller;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
-        $this->load->library( 'Template' );
-        $this->load->model( 'cms_model' );
+        $this->load->library('Template');
+        // $this->load->model('cms_model');
         $this->controller = $this->router->fetch_class();
     }
 
-    public function index() {
+    public function index()
+    {
         $data = array();
         $status = '';
         $msg = '';
-        $data['category'] = $this->cms_model->getLevelOneCategory();
-        $data['content'] = $this->cms_model->getPageContent( 'pd_charitable_trust' );
-        $data['msg'] = $this->template->getMessage( $status, $msg );
-        $this->template->setTitle( 'About Us' );
-        $this->template->setLayout( 'cms' );
-        $this->template->homeRender( 'cms/'.$this->controller.'/index', $data );
+        // $data['category'] = $this->cms_model->getLevelOneCategory();
+        // $data['content'] = $this->cms_model->getPageContent('pd_charitable_trust');
+        $data['msg'] = $this->template->getMessage($status, $msg);
+        $this->template->setTitle('Charity');
+        $this->template->setLayout('cms');
+        $this->template->homeRender('cms/' . $this->controller . '/index', $data);
     }
 
-    private function _sendEmailToAdmin( $contact_us = array() ) {
+    private function _sendEmailToAdmin($contact_us = array())
+    {
         $data = $contact_us;
         $to = ADMIN_EMAIL;
         $data['to_name'] = ADMIN_NAME;
         $subject = 'New Contact Us Request | Parrot Dipankar';
-        $body = $this->load->view( 'cms/'.$this->controller.'/email', $data, TRUE );
-        $this->tbl_generic_model->sendEmail( $to, $subject, $body, array(), array() );
+        $body = $this->load->view('cms/' . $this->controller . '/email', $data, TRUE);
+        $this->tbl_generic_model->sendEmail($to, $subject, $body, array(), array());
     }
-
 }
--- application/modules/cms/views/pd_charitable_trust/index.php
+++ application/modules/cms/views/pd_charitable_trust/index.php
@@ -3,21 +3,31 @@
         <h1 class="title">PD Charitable Trust</h1>
         <div class="breadcramb">
             <ul>
-                <li><a href="<?php echo base_url();?>"><i class="lnr lnr-home"></i></a></li>
+                <li><a href="<?php echo base_url(); ?>"><i class="lnr lnr-home"></i></a></li>
                 <li>Home</li>
             </ul>
         </div>
     </div>
 </section>
 
-<?php $this->load->view('cms/category');?>
-
 <section class="inner-layout">
     <div class="container">
         <div class="inner-content">
             <div class="row">
+                <!-- <div class="col-md-12 wrap-details-blog">
+                    <?php echo $content[0]->name_val; ?>
+                </div> -->
                 <div class="col-md-12 wrap-details-blog">
-                    <?php echo $content[0]->name_val;?>
+                    <p> <b> pd charitable</b></p>
+                    <p xss="removed"><b>My journey of farming</b> was started with local prison and Budgies, as and when
+                        I could manage some fund from parent or from CCTV business, I have added different more spices
+                        in my farm. Once I brought 2 African Grey Parrot. Those two lovely couple found my farm very
+                        homely and gave us 25 chicks. It made me crazier for birds. I started collecting more birds like
+                        Sunconure , Severe Macaw etc. I started selling those chicks, and earned more money. I used that
+                        money to increase my collection and enhance the farm capacity and atmosphere. This way my farm
+                        is very very colorful now.</p>
+                    <p xss="removed">With the support from my spouse, parents, younger brother, other family member and
+                        friends, now I am able to live with my passion.</p>
                 </div>
             </div>
             <div class="bottom-imglist">
