47a8bfba94e62fe24570255b4188c5b9334ce66b
sudipta <Sudipto-at-362131483632>
2020-10-18 09:38:29 +0530

    added design

--- application/modules/cms/views/event/details.php
+++ application/modules/cms/views/event/details.php
@@ -10,5 +10,170 @@
     </div>
 </section>
 
-<section class="inner-layout">
+<section class="inner-layout d-block mt-4 mb-4">
+    <div class="container">
+    <div class="row">
+    <div class="w-100 float-left ev-list-wrap">
+
+    <div class="col-12 col-md-12 col-lg-9 col-xl-9 float-left event-left p-0">
+
+    <div class="w-100 float-left ev-details">
+    <div class="w-100 float-left wrap-inner-event enent-details-wrap">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices. Vivamus sit amet pulvinar mi. Nam at placerat urna. Sed rutrum, ante eget fermentum sodales, est eros condimentum velit, nec consectetur lorem augue ac sapien. Morbi et arcu sit amet lacus ornare malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec blandit sem purus. Pellentesque quis magna odio, non mattis mi. In et dui mauris, sit amet ullamcorper nisl. </p><br>
+    <p>Duis a orci nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat ultrices elementum. Nullam nisi elit, semper nec eleifend et, auctor aliquet risus. Curabitur placerat lacus et orci blandit ac lacinia sem dignissim. Nam nec odio elit. Pellentesque dapibus commodo leo quis feugiat. In hac habitasse platea dictumst. Integer id tortor sit amet purus viverra aliquam nec ac elit. Fusce facilisis urna sed ligula pellentesque molestie. Duis ac risus elit.</p>
+    </div>
+    </div>
+    </div>
+
+    <div class="lists-ev-details w-100 float-left">
+    
+    <h2>See Other Events</h2>
+
+    <div class="w-100 blog-slider-owl-sud">
+                <div class="owl-carousel owl-theme owl-slider-event-details-sud">
+                    <div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+
+<div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="item">
+                         <div class="w-100 float-left box-event-lists">
+                            <div class="w-100 float-left wrap-inner-event">
+                            <a href="#" class="w-100 float-left">
+                            <div class="w-100 float-left img-ev">
+                            <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+                            </div>
+                            <div class="ev-content w-100 float-left">
+                            <h3>Event Title</h3>
+                            <span class="w-100 float-left">25th June 2020</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+                            </div>
+                            </a>
+                            </div>
+                            </div>
+                    </div>
+
+
+                    </div>
+</div>
+
+</div>
+
+    </div>
+
+    <div class="col-12 col-md-12 col-lg-3 col-xl-3 float-left event-right">
+
+    <div class="w-100 float-left recent-events">
+    <h4>Recent Events</h4>
+    <div class="w-100 float-left event-seps">
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+    </div>
+    </div>
+
+    <div class="w-100 float-left recent-events">
+    <h4>Upcoming Events</h4>
+    <div class="w-100 float-left event-seps">
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+    </div>
+    </div>
+
+    </div>
+
+    </div>
+
+
+    </div>
+    </div>
 </section>
--- application/modules/cms/views/event/list.php
+++ application/modules/cms/views/event/list.php
@@ -10,5 +10,195 @@
     </div>
 </section>
 
-<section class="inner-layout">
+<section class="inner-layout d-block mt-4 mb-4">
+    <div class="container">
+    <div class="row">
+    <div class="w-100 float-left ev-list-wrap">
+
+    <div class="col-12 col-md-12 col-lg-9 col-xl-9 float-left event-left p-0">
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id dolor dignissim vel vulputate eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus felis id...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    <div class="col-12 col-md-6 col-lg-4 col-xl-4 float-left box-event-lists">
+    <div class="w-100 float-left wrap-inner-event">
+    <a href="#" class="w-100 float-left">
+    <div class="w-100 float-left img-ev">
+    <img src="http://localhost/birds/uploads/blog/202006250426002.jpg" alt="">
+    </div>
+    <div class="ev-content w-100 float-left">
+    <h3>Event Title</h3>
+    <span class="w-100 float-left">25th June 2020</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  eros feugiat. Mauris accumsan aliquam ultrices...</p>
+    </div>
+    </a>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="col-12 col-md-12 col-lg-3 col-xl-3 float-left event-right">
+
+    <div class="w-100 float-left recent-events">
+    <h4>Recent Events</h4>
+    <div class="w-100 float-left event-seps">
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+    </div>
+    </div>
+
+    <div class="w-100 float-left recent-events">
+    <h4>Upcoming Events</h4>
+    <div class="w-100 float-left event-seps">
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+        <a href="#" class="w-100 float-left">Evet title will come here <i class="fas fa-angle-right"></i></a>
+    </div>
+    </div>
+
+    </div>
+
+    </div>
+
+
+    </div>
+    </div>
 </section>
