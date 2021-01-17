d114455dc98c38802b3bb250201675600cc7e51e
Ranaji Das <paltua@gmail.com>
2021-01-17 07:20:06 +0530

    added youtube link

--- application/views/theme1/common/socialLink.php
+++ application/views/theme1/common/socialLink.php
@@ -1,6 +1,6 @@
-
-	<li class="fb"><a target="_blank" href="https://m.facebook.com/Parrot-Dipankar-Family-238127920161260/?ref=bookmarks">Facebook</a></li>
-	<li class="twt"><a target="_blank" href="https://twitter.com/parrotdipankar">Twitter</a></li>
-	<li class="inst"><a target="_blank" href="https://www.instagram.com/parrotdipankar/">Instagram</a></li>
-	<li class="linkd"><a target="_blank" href="javascript:void(0)">Linkdin</a></li>
-	<li class="utube"><a target="_blank" href="https://www.youtube.com/c/PARROTDIPANKAR">YouTube</a></li>
+<li class="fb"><a target="_blank"
+        href="https://m.facebook.com/Parrot-Dipankar-Family-238127920161260/?ref=bookmarks">Facebook</a></li>
+<li class="twt"><a target="_blank" href="https://twitter.com/parrotdipankar">Twitter</a></li>
+<li class="inst"><a target="_blank" href="https://www.instagram.com/parrotdipankar/">Instagram</a></li>
+<li class="linkd"><a target="_blank" href="javascript:void(0)">Linkdin</a></li>
+<li class="utube"><a target="_blank" href="https://www.youtube.com/c/PARROTDIPANKAR/featured">YouTube</a></li>
